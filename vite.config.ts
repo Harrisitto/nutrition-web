7becf8cdb4dd77947b4ac289514d88da16081d7a
Josep Tomas i Juan <joseptomasjuan@gmail.com>
2026-05-02 17:52:55 +0200

    fix: reverted vite config and added CNAME file

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/nutrition-web/',
   plugins: [react(), visualizer({ open: true })],
   resolve: {
     alias: {
