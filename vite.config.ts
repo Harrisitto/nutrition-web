fc2857e5fd24e341b41f44c232da20b0e15e777e
Josep Tomas i Juan <joseptomasjuan@gmail.com>
2026-05-02 18:03:19 +0200

    hotfix: changed base url in viteconfig to match new domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/nutrition-web/',
+  base: '/',
   plugins: [react(), visualizer({ open: true })],
   resolve: {
     alias: {
