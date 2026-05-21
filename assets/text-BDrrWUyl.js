53e89758f0944ed305a90a8ede26ba21b01e6193
Josep Tomas i Juan <joseptomasjuan@gmail.com>
2026-05-21 19:54:21 +0200

    Updates

---
+++ assets/text-BDrrWUyl.js
@@ -0,0 +1,1 @@
+import{r as s}from"./index-D5DKn4ws.js";const u=({relativePath:t,extension:e="txt"})=>{const[r,c]=s.useState("");return s.useEffect(()=>{fetch(`/${t}.${e}`).then(x=>x.text()).then(c)},[t,e]),r};export{u};
