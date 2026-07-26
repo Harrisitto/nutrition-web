b5ca87594e2cf8dd7664653a8d398510b6f0ae8d
Josep Tomas i Juan <joseptomasjuan@gmail.com>
2026-07-26 18:23:05 +0200

    Updates

--- assets/validate-BdlcGqAC.js
+++
@@ -1,1 +0,0 @@
-const r=e=>e.trim()?"":"system:form.required",i=e=>/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(e)?"":"system:form.invalidEmail",n=(e,t)=>e.length<t?"system:form.minLength":"";export{i as V,r as a,n as b};
