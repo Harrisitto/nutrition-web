1d6a8827672f1687bde955ecd88bb6f7dacac243
Josep Tomas i Juan <joseptomasjuan@gmail.com>
2026-08-01 16:05:24 +0200

    Updates

--- assets/text-3QeSvVmH.js
+++
@@ -1,1 +0,0 @@
-import{r as s}from"./index-CFU6OA6T.js";const u=({relativePath:t,extension:e="txt"})=>{const[r,c]=s.useState("");return s.useEffect(()=>{fetch(`/${t}.${e}`).then(x=>x.text()).then(c)},[t,e]),r};export{u};
